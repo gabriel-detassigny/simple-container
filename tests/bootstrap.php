64dcfd7b9cae36e4ba5ccb758e661e16f1daa7f8
Gabriel de Tassigny <gabriel@multivac.cc>
2021-03-30 21:43:22 +0200

    Setup PHPCS config

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,21 +1,35 @@
 <?php
 
 namespace GabrielDeTassigny\SimpleContainer\Tests\Reflection {
-    class NoConstructor {}
-
-    class ConstructorNoParam {
-        public function __construct() {}
+    class NoConstructor
+    {
     }
 
-    class ConstructorWithClassParam {
-        public function __construct(\stdClass $param) {}
+    class ConstructorNoParam
+    {
+        public function __construct()
+        {
+        }
     }
 
-    class ConstructorWithDefaultParam {
-        public function __construct(?string $test = null) {}
+    class ConstructorWithClassParam
+    {
+        public function __construct(\stdClass $param)
+        {
+        }
     }
 
-    class ConstructorWithPrimitiveParam {
-        public function __construct(string $test) {}
+    class ConstructorWithDefaultParam
+    {
+        public function __construct(?string $test = null)
+        {
+        }
+    }
+
+    class ConstructorWithPrimitiveParam
+    {
+        public function __construct(string $test)
+        {
+        }
     }
 }
